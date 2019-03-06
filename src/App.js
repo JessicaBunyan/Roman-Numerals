5fee5abee6f9516398366840e91d0ab48df75804
Stan-Sasquatch <stephenppercival@gmail.com>
2019-03-06 21:47:03 +0000

    Updated UI, new issue when forms are emptied

--- src/App.js
+++ src/App.js
@@ -249,21 +249,27 @@ class App extends Component {
     constructor(props) {
       super(props);
       this.state = {romanText: '',
-    numberText: ''};
+    numberText: '',
+romanTextConverted: '',
+numberTextConverted: ''};
   
       this.handleRomanChange = this.handleRomanChange.bind(this);
       this.handleNumberChange = this.handleNumberChange.bind(this);
-      this.handleSubmitToRoman = this.handleSubmitToRoman.bind(this);
+      
+      /*this.handleSubmitToRoman = this.handleSubmitToRoman.bind(this);
       this.handleSubmitToArabic = this.handleSubmitToArabic.bind(this);
+      */
     }
   
     handleRomanChange(event) {
-      this.setState({romanText: event.target.value});
+      this.setState({romanText: event.target.value,
+    numberTextConverted: this.convertToArabicNum(event.target.value)});
     }
     handleNumberChange(event) {
-        this.setState({numberText: event.target.value});
+        this.setState({numberText: event.target.value,
+        romanTextConverted: this.convertToRoman(event.target.value) });
       }
-  
+  /*
     handleSubmitToRoman(event) {
       alert('The roman numeral is: ' + this.convertToRoman(this.state.numberText));
       event.preventDefault();
@@ -273,36 +279,28 @@ class App extends Component {
         alert('The Arabic number is: ' + this.convertToArabicNum(this.state.romanText));
         event.preventDefault();
       }
-  
-    /*render() {
-      return (
-        <form onSubmit={this.handleSubmitToRoman}>
-          <label>
-            number:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
-    }*/
+  */
+    
     render() {
         return (
             <div>
-          <form onSubmit={this.handleSubmitToArabic}>
+          <form >
             <label>
               Roman Numeral:
               <input type="text" value={this.state.romanText} onChange={this.handleRomanChange} />
             </label>
-            <input type="submit" value="Submit" />
+            
           </form>
           
-         <form onSubmit={this.handleSubmitToRoman}>
+         <form>
           <label>
             Number:
             <input type="text" value={this.state.numberText} onChange={this.handleNumberChange} />
           </label>
-          <input type="submit" value="Submit" />
+        
         </form>
+        <h1>The Arabic number is: {this.state.romanTextConverted}</h1>
+        <h2>The roman numeral is:  {this.state.numberTextConverted}</h2>
         </div>
         );
       }
